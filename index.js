3f01267979a1f1b55f9e7f80a631c7c3fc1cc619
Ryan Horn <rj.horn@gmail.com>
2022-11-25 16:12:43 -0700

    commit tree a208ea0866a7d796bb340bd04312f7809dc1a76a

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(pickUpLoc){
+    return Math.abs(42-pickUpLoc)
+}
+
+function distanceFromHqInFeet(streetLoc){
+    //each block is 264 feet
+    let distBlocks = Math.abs(42-streetLoc)
+    return distBlocks * 264
+}
+
+function distanceTravelledInFeet(startBlockNo, endBlockNo){
+    let distBlocks = Math.abs(startBlockNo - endBlockNo)
+    return distBlocks * 264
+}
+
+
+function calculatesFarePrice(start, destination){
+
+    let distBlocks = Math.abs(start - destination)
+    let distFeet = distBlocks * 264
+    let distFeetOverFourHun = distFeet - 400
+    
+    
+    if(distFeet <= 400){
+        return 0
+    }
+    if(distFeet >= 2500){
+        return 'cannot travel that far'
+    }
+    if(distFeet > 400 && distFeet < 2000){
+        return 0.02 * distFeetOverFourHun
+    }
+    if(distFeet >= 2000 && distFeet < 2500 ){
+        return 25
+    }
+    
+
+}
+
+
+
